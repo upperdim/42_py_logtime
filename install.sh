aa79b91aaaf5bc0aa28027c6c788b84350e21a70
upperdim <45048352+upperdim@users.noreply.github.com>
2024-02-13 02:13:12 +0100

    more installation messages

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 echo "Installing 42_py_logtime..."
+echo "Creating Python virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
+echo "Installing dependencies..."
 pip install -r requirements.txt
+echo "Giving execution permission to logtime.sh..."
 chmod +x logtime.sh
 echo "UID = \"your 42 API UID from intranet API page\"\nSECRET = \"your 42 API SECRET from intranet API page\"\nUSERNAME = \"your intra nickname\"" > credentials.py
 echo "credentials.py file created, please fill it with your 42 API credentials as explained in the README."
